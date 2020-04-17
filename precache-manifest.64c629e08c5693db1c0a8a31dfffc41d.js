321bd0c1e50f00beaae6e534c32f725dfae74610
Julie Nginn <nginn.julie@gmail.com>
2020-04-17 17:15:36 +0200

    Updates

---
+++ precache-manifest.64c629e08c5693db1c0a8a31dfffc41d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4abb8342f5e153ef576d552573837b1a",
+    "url": "/my-tabata/index.html"
+  },
+  {
+    "revision": "560c2c13373220a8c566",
+    "url": "/my-tabata/static/js/2.739a5cd7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-tabata/static/js/2.739a5cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1326f097c3a002e466a3",
+    "url": "/my-tabata/static/js/main.ef2b9b7d.chunk.js"
+  },
+  {
+    "revision": "db19b08e98cc186e63ca",
+    "url": "/my-tabata/static/js/runtime-main.04489472.js"
+  },
+  {
+    "revision": "ab8be1a00a8eff7f5444b39108a1c273",
+    "url": "/my-tabata/static/media/end.ab8be1a0.mp3"
+  },
+  {
+    "revision": "5d4a4d6fc168957a24fb83431a30360d",
+    "url": "/my-tabata/static/media/logo.5d4a4d6f.png"
+  },
+  {
+    "revision": "9b1549b64f9b1ee8272952b460508db5",
+    "url": "/my-tabata/static/media/prepare.9b1549b6.mp3"
+  },
+  {
+    "revision": "787cd1bdb5989fbe66b8bbf59aad0d1f",
+    "url": "/my-tabata/static/media/rest.787cd1bd.mp3"
+  },
+  {
+    "revision": "f5c210b052b7ba5221dfb0147b474b0d",
+    "url": "/my-tabata/static/media/run.f5c210b0.mp3"
+  },
+  {
+    "revision": "ff7b361d0d7c89577002b12b266fae33",
+    "url": "/my-tabata/static/media/work.ff7b361d.mp3"
+  }
+]);
